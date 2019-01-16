29c71c0a0bbb71b05fd8962494f41f5440ed3a1a
mirrsan <mirsan.kajovic@bild-studio.net>
2019-01-16 11:10:44 -0300

    swagger

---
+++ frrstBildStudioProject/Controllers/TodoController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using frrstBildStudioProject.Models;
+
+namespace TodoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+        // get metod
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+        // post metod
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+        /// <summary>
+        ///   <remaks>
+        ///      POST /Todo
+        ///      {
+        ///           "id" : 1,
+        ///           "name" : "Item1",
+        ///           "isComplete" : true
+        ///       }
+        ///  </remaks>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code = "201">returns the newly created item</respomse>
+        /// <response code = "400">If the item is null</response>
+
+        // delete metod
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return todoItem;
+        }
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult<TodoItem> Create(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+
+    }
+}
